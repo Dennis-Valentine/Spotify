3f94327ce4a81cda9e926f5ae7681befbd8dce3b
Dennis Valentine <denden.emails@gmail.com>
2021-02-22 00:29:11 +0000

    cleaning script pipeline

---
+++ R/spotify_user_details_template.R
@@ -0,0 +1,23 @@
+
+# Background --------------------------------------------------------------
+
+# Use this as a template. 
+
+
+# Details -----------------------------------------------------------------
+cli::cli_h1(text = "Loading in user credentials")
+
+# Client ID
+id <- "" 
+# Client Secret 
+secret <- ""
+
+# username
+my_id <- ""
+
+# OAutho token - Make sure the access token is up to date, LOL. 
+# https://developer.spotify.com/console/delete-current-user-saved-tracks/?ids=4iV5W9uYEdYUVa79Axb7Rh
+cli::cli_alert_danger(text = "Request a new token: https://developer.spotify.com/console/delete-current-user-saved-tracks/?ids=4iV5W9uYEdYUVa79Axb7Rh")
+cli::cli_alert_warning(text = "access_token <- 'access_token'")
+access_token <- ""
+
